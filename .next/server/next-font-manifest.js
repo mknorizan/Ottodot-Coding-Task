2f739133338b96e3a60ff1dd99d9675f802bd310
Khairi Norizan <NorizanKhairi@JohnDeere.com>
2025-10-06 00:25:57 -0500

    Delete .next folder

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Volumes/K'S SANDISK/Job Application/Ottodot/Ottodot-Coding-Task/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
